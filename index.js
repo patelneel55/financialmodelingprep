995b723a5315e49933714882c6eb3b1114f9ddb7
Neel Patel <patelneel55@gmail.com>
2020-01-22 17:01:13 -0500

    Add util functions to make request

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 'use strict'
 
+let Data = require('./lib/data')
+
 // module.exports = (config = {}) => {
 console.log("hello");
 // Add the base url for submodules to use.
-config.base = `https://financialmodelingprep.com/api/v3/`
+let cc = {};
+cc["base"] = `https://financialmodelingprep.com/api/v3/`
+
+let dd = Data(cc);
+
+dd.quote('AAPL')
+
 // };
