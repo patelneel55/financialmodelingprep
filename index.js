56fa48c0771f5c9f106aac7c070ab7d84f5d1509
Neel Patel <patelneel55@gmail.com>
2020-01-22 15:32:56 -0500

    Setup package structure

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+// module.exports = (config = {}) => {
+console.log("hello");
+// Add the base url for submodules to use.
+config.base = `https://financialmodelingprep.com/api/v3/`
+// };
