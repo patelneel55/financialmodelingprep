6dd026285ba8e7a44a02536b5d1f27df4b1190e6
Neel Patel <patelneel55@gmail.com>
2020-01-25 17:33:40 -0500

    Integrate chai and add example tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,42 @@
-const assert = require('assert');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 
-describe('Test', () => {
-    describe('#valueEquals', () => {
-        it('should be 4 when 2+2 is called', () => {
-            assert.equal(2+2, 4);
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+const stock = require('../lib/stock');
+
+describe('.stock', () => {
+    describe('.profile', () => {
+        it('should be 4 when 2+2 is called', (done) => {
+            // chai.request('https://financialmodelingprep.com/api/v3')
+            // .get('/company/profile/AAPL')
+            // .end((err, res) => {
+            //     stock('AAPL').quote().then((response) => {
+            //         ///expect(response).to.deep.equal(res);
+            //         // assert.equal(2+2, 5);
+            //         // done()
+            //     }).catch((err)=>console.log(err));
+            //     // assert.equal(2+2, 5);
+            //     // done()
+            // });
+            // assert.equal(2+2, 4);
+            // stock('MSFT').quote().then(res => {
+                
+            //     expect(2+2).to.equal(4);
+            //     done();
+            // })
+            chai.request('https://financialmodelingprep.com/api/v3')
+            .get('/company/profile/AAPL')
+            .end((err, res) => {
+                stock('MSFT').quote()
+                .then((response) => {
+                    expect(2+2).to.equal(5);
+                    done();
+                })
+                .catch(err => done(err));
+            })
+            
         })
     })
 })
