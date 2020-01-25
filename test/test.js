f89b22a82b76198df6ba50b1ba2bcab2460a7a2a
Neel Patel <patelneel55@gmail.com>
2020-01-25 16:45:29 -0500

    Add mocha testing framework

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('Test', () => {
+    describe('#valueEquals', () => {
+        it('should be 4 when 2+2 is called', () => {
+            assert.equal(2+2, 4);
+        })
+    })
+})
