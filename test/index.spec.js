cbfbcb4a4097743e0325a73aa565e6aa866a1942
Neel Patel <patelneel55@gmail.com>
2020-02-03 20:54:18 -0500

    Add and fix market and index tests

---
+++ test/index.spec.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+const index = require('../lib/index.js');
+
+describe('.market', () => {
+    describe('.index', () => {
+        describe('.list', () => {
+            it('should return valid data', (done) => {
+                chai.request('https://financialmodelingprep.com/api/v3')
+                    .get('/majors-indexes')
+                    .end((err, res) => {
+                        index().list()
+                            .then((response) => {
+                                expect(res.body).to.eql(response);
+                                done();
+                            })
+                            .catch(done);
+                    })
+            });
+    
+            it('invalid parameter should return valid data', (done) => {
+                chai.request('https://financialmodelingprep.com/api/v3')
+                    .get('/majors-indexes')
+                    .end((err, res) => {
+                        index().list()
+                            .then((response) => {
+                                expect(res.body).to.eql(response);
+                                done();
+                            })
+                            .catch(done);
+                    })
+            });
+        });
+    });
+});
